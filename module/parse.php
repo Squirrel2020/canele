00f99872cac1613b743f8322c1f70d0788bf60f1
Squirrel2020 <squirrel2020@mail.ru>
2016-04-03 06:31:16 +0300

    Update parse.php

--- module/parse.php
+++ module/parse.php
@@ -40,35 +40,8 @@ include_once('simple_html_dom.php');
 
 // Создаем объект DOM на основе кода, полученного по ссылке
 
-$html = file_get_html($_GET['bob']);
+$html = file_get_html($_GET['perpetual']);
 
-
-
-echo "Rem ";
-echo "`";
-echo ";";
-// Текст цены объявления
-foreach($html->find('div[class=item_similars_p]') as $key=>$element)
-{
-${'A' . $key} = $element->innertext;
-}
-//echo $A0;
-$pricetag00 = str_replace("&nbsp;", "", $A0);
-$pricetag01 = str_replace(" ", "", $pricetag00);
-$pricetag02 = str_replace(".", "", $pricetag01);
-$pricetag03 = str_replace("р", "", $pricetag02);
-echo $pricetag03;
-echo ";";
-echo $_GET['bob'];
-echo "`";
-// Текст заголовка объявления
-foreach($html->find('h1[itemprop=name]') as $element) 
-echo $element->innertext;
-echo "`";
-echo $_GET['t'];
-echo "`";
-
-// Текст описания объявления
 foreach($html->find('div[id=desc_text]') as $element) 
 //echo $element->innertext;
 $bodytag01 = str_replace("<p>", "", $element->innertext);
@@ -77,8 +50,86 @@ $bodytag03 = str_replace("</p>", "", $bodytag02);
 //$bodytag03s = substr_replace($bodytag03, 0, 3);
 $bodytag03s = stristr($bodytag03, "br");
 $bodytag03ss = substr_replace($bodytag03s, null, 0, 3);
-echo $bodytag03ss;
-echo "`";
+$bodytag03swag = str_replace("<BR>", "atgs", $bodytag03ss);
+$bodytag03satg = str_replace("\t", " ", $bodytag03swag);
+//$bodytag03satgx = preg_replace ("/[^a-zA-ZА-Яа-я0-9\s]/","",$bodytag03satg);
+$string = strtolower(preg_replace('/[^a-zA-ZА-Яа-я0-9\s]/',' ',iconv('UTF-8','cp1251',$bodytag03satg)));
+$string = iconv('cp1251','UTF-8',$string);
+$string = str_replace("atgs", "\\n", $string);
+//echo $string;
+if ($string=="") {exit(1);}else{ }
+
+
+//echo "Rem ";
+//echo "`";
+//echo ";";
+// Текст цены объявления
+//foreach($html->find('div[class=item_similars_p]') as $key=>$element)
+//{
+//${'A' . $key} = $element->innertext;
+//}
+////echo $A0;
+//$pricetag00 = str_replace("&nbsp;", "", $A0);
+//$pricetag01 = str_replace(" ", "", $pricetag00);
+//$pricetag02 = str_replace(".", "", $pricetag01);
+//$pricetag03 = str_replace("р", "", $pricetag02);
+//echo $pricetag03;
+//echo ";";
+//echo $_GET['bob'];
+//echo "`";
+
+
+foreach($html->find('a[class=breadcrumb-link]') as $key=>$element)
+{
+${'F' . $key} = $element->innertext;
+}
+//{\"data\":\"https://www.avito.ru/additem\",\"evType\":\"redirect\",\"newValue\":\"\"},
+echo "{\"action\":[{\"data\":\"document.getElementsByClassName(\\\"col-3 form-section__title\\\")\\n[0].innerHTML = \\\"";
+echo $F3;
+echo " | ";
+echo $F4;
+echo " | ";
+echo $F5;
+echo "\\\";\",";
+echo "\"evType\":\"inject\",\"newValue\":\"\"},{\"data\":\"span:contains('";
+if ($F1=="") {echo "_";}else{echo $F1;}
+echo "')\",\"evType\":\"click\",\"newValue\":\"\"},{\"data\":\"span:contains('";
+if ($F2=="") {echo "_";}else{echo $F2;}
+echo "')\",\"evType\":\"click\",\"newValue\":\"\"},{\"data\":\"span:contains('";
+if ($F3=="") {echo "_";}else{echo $F3;}
+echo "')\",\"evType\":\"click\",\"newValue\":\"\"},{\"data\":\"#item-edit__title\",\"evType\":\"change\",\"newValue\":\"";
+// ================== Текст заголовка объявления
+echo " ";
+// ================== Текст заголовка объявления
+echo "\"},";
+//echo "\",\"msgType\":\"userEvent\"},";
+echo "{\"data\":\"#item-edit__description\",\"evType\":\"change\",\"newValue\":\"";
+// ================== Текст описания объявления
+foreach($html->find('h1[itemprop=name]') as $element) 
+echo $element->innertext;
+echo "\\n";
+foreach($html->find('div[id=desc_text]') as $element) 
+//echo $element->innertext;
+$bodytag01 = str_replace("<p>", "", $element->innertext);
+$bodytag02 = str_replace("<br />", "<BR>", $bodytag01);
+$bodytag03 = str_replace("</p>", "", $bodytag02);
+//$bodytag03s = substr_replace($bodytag03, 0, 3);
+$bodytag03s = stristr($bodytag03, "br");
+$bodytag03ss = substr_replace($bodytag03s, null, 0, 3);
+$bodytag03swag = str_replace("<BR>", "atgs", $bodytag03ss);
+$bodytag03satg = str_replace("\t", " ", $bodytag03swag);
+//$bodytag03satgx = preg_replace ("/[^a-zA-ZА-Яа-я0-9\s]/","",$bodytag03satg);
+$string = strtolower(preg_replace('/[^a-zA-ZА-Яа-я0-9\s]/',' ',iconv('UTF-8','cp1251',$bodytag03satg)));
+$string = iconv('cp1251','UTF-8',$string);
+$string = str_replace("atgs", "\\n", $string);
+echo $string;
+
+
+
+
+
+
+
 
 // Изображения объявления
 
@@ -95,6 +146,15 @@ $gen4 = generateRandomString();
 $gen5 = generateRandomString();
 $gen6 = generateRandomString();
 
+echo "\\n";
+echo "..\\\\" . $gen1;
+// ================== Текст описания объявления
+echo "\",\"msgType\":\"userEvent\"},{\"data\":\"#item-edit__price\",\"evType\":\"change\",\"newValue\":\"";
+echo $_GET['t'];
+echo "\",\"msgType\":\"userEvent\"}],\"name\":\"";
+echo $fdat;
+echo "\",\"level\":\"1\",\"isLeaf\":true,\"expanded\":false,\"loaded\":true},";
+echo "\n\n\n\n\n";
 
 echo $gen1;
 echo ".jpg";
@@ -140,22 +200,10 @@ echo "`";
 //echo $abx5;
 //echo "`";
 
-foreach($html->find('a[class=breadcrumb-link]') as $key=>$element)
-{
-${'F' . $key} = $element->innertext;
-}
 
-echo $F1;
 echo "`";
-echo $F2;
+echo "C:\\Users\\1\\gbot\\1\\" . $gen1;
 echo "`";
-echo $F3;
-echo "`";
-echo $F4;
-echo "`";
-echo $F5;
-echo "`";
-
 
 echo "\n";
 echo "IF EXIST renmode ( ren \"%~nx0\" ".$fdat.".bat ) ELSE ( goto mainalgo )\n";
@@ -164,6 +212,7 @@ echo ":mainalgo\n";
 
 echo "\n";
 echo "\n";
+echo "mkdir " . $gen1;
 echo "\n";
 //echo $A0;
 $abs0 = substr($A0, -14);
@@ -171,10 +220,13 @@ $imgtag00 = str_replace("//", "wget --no-check-certificate https://", $A0);
 echo $imgtag00 . " -O " . $abs0;
 //renaming AIVOT shit to refrain from filename recognition
 
+
 echo "\n";
 echo "convertk -shave 1x51 ";
 echo $abs0;
 echo " ";
+echo $gen1;
+echo "\\";
 echo $gen1;
 echo ".jpg";
 echo "\n";
@@ -188,6 +240,8 @@ echo "\n";
 echo "convertk -shave 1x51 ";
 echo $abs1;
 echo " ";
+echo $gen1;
+echo "\\";
 echo $gen2;
 echo ".jpg";
 echo "\n";
@@ -201,6 +255,8 @@ echo "\n";
 echo "convertk -shave 1x51 ";
 echo $abs2;
 echo " ";
+echo $gen1;
+echo "\\";
 echo $gen3;
 echo ".jpg";
 echo "\n";
@@ -214,6 +270,8 @@ echo "\n";
 echo "convertk -shave 1x51 ";
 echo $abs3;
 echo " ";
+echo $gen1;
+echo "\\";
 echo $gen4;
 echo ".jpg";
 echo "\n";
@@ -227,6 +285,8 @@ echo "\n";
 echo "convertk -shave 1x51 ";
 echo $abs4;
 echo " ";
+echo $gen1;
+echo "\\";
 echo $gen5;
 echo ".jpg";
 echo "\n";
@@ -240,6 +300,8 @@ echo "\n";
 echo "convertk -shave 1x51 ";
 echo $abs5;
 echo " ";
+echo $gen1;
+echo "\\";
 echo $gen6;
 echo ".jpg";
 echo "\n";
@@ -273,8 +335,8 @@ echo $L1;
 
 
 echo "\n";
-echo "iconv -f utf-8 -t cp1251 %~nx0>";
-
+//echo "iconv -f utf-8 -t cp1251 %~nx0>";
+echo "type %~nx0>";
 
 echo $fdat;
 
@@ -284,7 +346,7 @@ echo ".csv\n\n\n\n";
 
 
 
-echo "EXIT /B\n\n\n";
+//echo "EXIT /B\n\n\n";
 
 
 ?>
